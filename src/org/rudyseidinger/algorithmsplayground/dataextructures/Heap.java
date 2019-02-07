42d494404379ce7d430ba57337f5e99f60689c9d
Rudy Seidinger <rudyseidinger@gmail.com>
2019-02-07 09:41:34 +0000

    Heap first stab

---
+++ src/org/rudyseidinger/algorithmsplayground/dataextructures/Heap.java
@@ -0,0 +1,56 @@
+package org.rudyseidinger.algorithmsplayground.dataextructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("rawtypes")
+class Heap<T> {	
+	private Heap parentNode;
+	private List<Heap> childNodes;
+	private T object;
+		
+	public Heap(T object) {
+		this.setObject(object);
+		this.setChildNodes(new ArrayList<Heap>());
+	}
+	
+	public Heap(T object, Heap parentNode) {
+		this.setObject(object);
+		this.setParentNode(parentNode);
+	}
+	
+	public Heap(T object, Heap parentNode, List<Heap> childNodes) {
+		this.setObject(object);
+		this.setParentNode(parentNode);
+		this.setChildNodes(childNodes);
+		this.sortTree();
+	}
+
+	private void sortTree() {
+		
+	}
+
+	public T getObject() {
+		return object;
+	}
+
+	public void setObject(T object) {
+		this.object = object;
+	}
+
+	public Heap getParentNode() {
+		return parentNode;
+	}
+
+	public void setParentNode(Heap parentNode) {
+		this.parentNode = parentNode;
+	}
+
+	public List<Heap> getChildNodes() {
+		return childNodes;
+	}
+
+	private void setChildNodes(List<Heap> childNodes) {
+		this.childNodes = childNodes;
+	}
+}
---
+++ src/org/rudyseidinger/algorithmsplayground/tests/HeapTest.java
@@ -0,0 +1,24 @@
+package org.rudyseidinger.algorithmsplayground.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeapTest {
+
+	@Test
+	public void testHeapT() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHeapTHeap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHeapTHeapListOfHeap() {
+		fail("Not yet implemented");
+	}
+
+}
